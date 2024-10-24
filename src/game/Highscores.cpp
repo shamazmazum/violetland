0e6a2542a2f260d37b380be93622cdfd329bcd50
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-10-24 09:22:33 +0300

    Compatible with newer boost

--- src/game/Highscores.cpp
+++ src/game/Highscores.cpp
@@ -105,7 +105,7 @@ bool Highscores::add(HighscoresEntry entry) {
 		ofile.close();
 
 		boost::filesystem::copy_file(hsTempFile, hsFile, 
-				boost::filesystem::copy_option::overwrite_if_exists);
+				boost::filesystem::copy_options::overwrite_existing);
 		boost::filesystem::remove(hsTempFile);
 
 		cout << "Scores was updated." << endl;
